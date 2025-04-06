330fa10d8a0780b619f8bb02e9c53be44087c079
luanacamaral2012 <luanacamaral2012@gmail.com>
2025-04-06 00:55:28 +0000

    indo pra reta final

--- .vscode/main.js
+++ .vscode/main.js
@@ -1,18 +1,42 @@
-let bill = 0
-let tipPercentage = 0
-let numberOfpeople = 0
-let buttonSelected = null
+let bill = 0;
+let tipPercentage = 0;
+let numberOfpeople = 0;
+let buttonSelected = null;
+let isTypingCustom = false;
 
-function receiveBillValue(){
-    bill =document.querySelector("bill").valueAsNumber
+function receiveBillValue() {
+    bill = document.querySelector("#bill").valueAsNumber;
 }
-function receiveNumberOfPeopleValue(){
-    numberOfpeople = document.querySelector("#people").valueAsNumber
+
+function receiveNumberOfPeopleValue() {
+    numberOfpeople = document.querySelector("#people").valueAsNumber;
 } 
 
-function receiveTipPercentageValue(value){
-tipPercentage = value/100
+function receiveTipPercentageValue(value) {
+    tipPercentage = value / 100;
 
-buttonSelected = document.querySelector(`#button-${value}`)
-buttonSelected.classList.add("button-selected")
+    removeClassButtonSelected();
+
+    buttonSelected = document.querySelector(`#button-${value}`);
+    buttonSelected.classList.add("button-selected");
+
+    if (!isTypingCustom) {
+        document.querySelector("#custom-tip").value = "";
+    }
+
+    isTypingCustom = false;
+} 
+
+function receiveCustomTipPercentageValue() {
+    tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100;
+
+    removeClassButtonSelected();
+    isTypingCustom = true;
+}
+
+function removeClassButtonSelected() {
+    if (buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected");
+        buttonSelected = null;
+    }
 }
