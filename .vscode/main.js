8de48a3b5b439c2a114df0d7949ce778c4e761fc
luanacamaral2012 <luanacamaral2012@gmail.com>
2025-04-06 00:03:08 +0000

    trabalhando java

--- .vscode/main.js
+++ .vscode/main.js
@@ -1,10 +1,18 @@
 let bill = 0
 let tipPercentage = 0
-let numberOfpeople = 0 
+let numberOfpeople = 0
+let buttonSelected = null
 
 function receiveBillValue(){
     bill =document.querySelector("bill").valueAsNumber
 }
 function receiveNumberOfPeopleValue(){
-    
+    numberOfpeople = document.querySelector("#people").valueAsNumber
+} 
+
+function receiveTipPercentageValue(value){
+tipPercentage = value/100
+
+buttonSelected = document.querySelector(`#button-${value}`)
+buttonSelected.classList.add("button-selected")
 }
